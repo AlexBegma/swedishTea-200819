6a9f126681ea6fb29002afb9c31231d15e1a110b
alexbehma <alexbegma@gmail.com>
2019-08-21 22:00:33 +0300

    section list

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(function(){
 		return false;
 	});
 
+// ----- hover effect ----
+	/*	$('.list__inner .tab').hover(function(){
+		var id = $(this).attr('data-id');
+		$('.list__inner').find('.tab-item').removeClass('active-tab').hide();
+		$('.list__inner .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	}); */
+
 });
